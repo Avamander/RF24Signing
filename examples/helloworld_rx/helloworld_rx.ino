b84ed8de2c5791f11c7a1c9640443284bb93ff98
Avamander <avamander@gmail.com>
2017-01-22 23:51:07 +0200

    Added some experimental examples.

---
+++ examples/helloworld_rx/helloworld_rx.ino
@@ -0,0 +1,59 @@
+/*
+  Copyright (C) 2012 James Coliz, Jr. <maniacbug@ymail.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  version 2 as published by the Free Software Foundation.
+
+  Update 2014 - TMRh20
+*/
+
+/**
+   Simplest possible example of using RF24Network,
+
+   RECEIVER NODE
+   Listens for messages from the transmitter and prints them out.
+*/
+
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+
+#include "sha256.h"
+Sha256Class Sha256;
+
+RF24 radio(9, 10);                // nRF24L01(+) radio attached using Getting Started board
+RF24Network network(radio);      // Network uses that radio
+
+const uint16_t this_node = 00;    // Address of our node in Octal format ( 04,031, etc)
+const uint16_t other_node = 01;   // Address of the other node in Octal format
+
+#include "RF24Signing.h"
+
+RF24NetworkHeader header;
+
+
+
+int sensor_id;
+int sensor_data;
+
+void setup(void) {
+  Serial.begin(115200);
+  Serial.println("RF24Network/examples/helloworld_rx/");
+
+  SPI.begin();
+  radio.begin();
+  network.begin(/*channel*/ 90, /*node address*/ this_node);
+}
+
+void loop(void) {
+  network.update();                  // Check the network regularly
+  SignedNetworkMaintenance();
+
+  while (UnsignedNetworkAvailable(&sensor_id, &sensor_data)) {
+    Serial.print("Unsigned Sensor ID: ");
+    Serial.println(sensor_id);
+    Serial.print("Unsigned Sensor DATA: ");
+    Serial.println(sensor_data);
+  }
+}
---
+++ examples/helloworld_tx/helloworld_tx.ino
@@ -0,0 +1,77 @@
+/*
+  Copyright (C) 2012 James Coliz, Jr. <maniacbug@ymail.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  version 2 as published by the Free Software Foundation.
+
+  Update 2014 - TMRh20
+*/
+
+/**
+   Simplest possible example of using RF24Network
+
+   TRANSMITTER NODE
+   Every 2 seconds, send a payload to the receiver node.
+*/
+
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+
+
+RF24 radio(9, 10);
+
+#include "sha256.h"
+Sha256Class Sha256;
+
+RF24Network network(radio);          // Network uses that radio
+const uint16_t this_node = 01;        // Address of our node in Octal format
+const uint16_t other_node = 00;       // Address of the other node in Octal format
+
+const unsigned long interval = 500;//ms  // How often to send 'hello world to the other unit
+
+unsigned long last_sent = 60000;            // When did we last send?
+unsigned long packets_sent;          // How many have we sent already
+
+
+#include "RF24Signing.h"
+
+RF24NetworkHeader header;
+
+int sensor_id;
+int sensor_data;
+
+struct payload_s {
+  uint8_t hash[32];
+  int sensor_id;
+  int sensor_data;
+};
+
+struct payload_s payload;
+
+void setup(void){
+  Serial.begin(115200);
+  Serial.println("RF24Network/examples/helloworld_tx/");
+  Sha256.init();
+  SPI.begin();
+  radio.begin();
+  network.begin(/*channel*/ 90, /*node address*/ this_node);
+}
+
+void loop() {
+  network.update();                          // Check the network regularlys
+
+  unsigned long now = millis();              // If it's time to send a message, send it!
+  if ( now - last_sent >= interval  ) {
+    last_sent = now;
+    Serial.print("Main loop: Sending...");
+  }
+
+  while (UnsignedNetworkAvailable()) {
+    
+  }
+}
+
+
+
