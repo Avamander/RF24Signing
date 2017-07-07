cf12042faa2b2e686e16f0237c48fe6ac9ff72b3
Avamander <avamander@gmail.com>
2017-07-07 22:00:37 +0300

    Added doxygen documentation.

---
+++ hmacs.c
@@ -0,0 +1,25 @@
+/*
+    A transparent signing library for RF24Mesh
+    Copyright (C) 2017 Avamander <avamander@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <stdint.h>
+#include <Arduino.h>
+
+static const uint8_t hmacs[2][20] PROGMEM = {
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+};
